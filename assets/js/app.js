584adfee93cc6e273a6a7318e743ba1cff7ecd4a
luisa mayta <luisa.mayta.1212@gmail.com>
2017-10-02 08:44:01 -0700

    subiendo mis abances

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+            $("img").click(function(){
+                $(this).hide();
+            });
+    
+           $("img").click(function(){
+                $(this).hide();
+            });
+            
+});
+
+
